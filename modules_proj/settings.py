bd3be2223ef80209e2ff1b63b66f7470035d0ae7
srbombaywala <srbombaywala@gmail.com>
2024-02-23 11:25:14 +0530

    installed psycopg2

--- modules_proj/settings.py
+++ modules_proj/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'modules_proj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'da6e-G2Ac4-AfAGCgAcd4514fGG1Ef5b',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '18212',
     }
 }
 
